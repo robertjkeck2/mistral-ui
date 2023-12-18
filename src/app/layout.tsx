9496816261658ef246321a503b3efca13d4c053a
John Keck <robertjkeck2@gmail.com>
2023-12-18 17:42:58 -0600

    Continued setup with shad

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,7 @@
 import type { Metadata } from "next";
-import { Inter as FontSans } from "next/font/google";
 import "@/styles/globals.css";
 import { cn } from "@/lib/utils";
-
-export const fontSans = FontSans({
-  subsets: ["latin"],
-  variable: "--font-sans",
-});
+import { fontSans } from "@/lib/fonts";
 
 export const metadata: Metadata = {
   title: "Mistral UI",
@@ -20,7 +15,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={cn("min-h-screen bg-background font-sans antialiased")}>
+      <body
+        className={
+          (cn("min-h-screen bg-background font-sans antialiased"),
+          fontSans.variable)
+        }
+      >
         {children}
       </body>
     </html>
