0fdfa35ec10fb7f107cf2d381bf66931f80cf232
John Keck <robertjkeck2@gmail.com>
2023-12-19 11:31:35 -0600

    Copy playground from shadcn demo page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,27 +2,40 @@ import type { Metadata } from "next";
 import "@/styles/globals.css";
 import { cn } from "@/lib/utils";
 import { fontSans } from "@/lib/fonts";
+import { ThemeProvider } from "@/components/providers";
 
 export const metadata: Metadata = {
   title: "Mistral UI",
   description: "Next.js UI for Mistral AI platform",
 };
 
-export default function RootLayout({
-  children,
-}: {
+interface RootLayoutProps {
   children: React.ReactNode;
-}) {
+}
+
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <html lang="en">
-      <body
-        className={
-          (cn("min-h-screen bg-background font-sans antialiased"),
-          fontSans.variable)
-        }
-      >
-        {children}
-      </body>
-    </html>
+    <>
+      <html lang="en" suppressHydrationWarning>
+        <head />
+        <body
+          className={cn(
+            "min-h-screen bg-background font-sans antialiased",
+            fontSans.variable
+          )}
+        >
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <div className="relative flex min-h-screen flex-col">
+              <div className="flex-1">{children}</div>
+            </div>
+          </ThemeProvider>
+        </body>
+      </html>
+    </>
   );
 }
