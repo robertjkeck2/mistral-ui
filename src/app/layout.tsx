2d25afb70d04981578dd0119193ff816ceb2a6f8
John Keck <robertjkeck2@gmail.com>
2023-12-20 16:30:26 -0600

    Default to dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
         >
           <ThemeProvider
             attribute="class"
-            defaultTheme="system"
+            defaultTheme="dark"
             enableSystem
             disableTransitionOnChange
           >
